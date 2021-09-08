1ce8af715368981e1e82c30d2d0b498b30c4bbec
hadeeny <oluwasegunadeniyi064@gmail.com>
2021-09-08 16:17:09 -0700

    mobile design

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleNav = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
+
+const name = "Segun Adeniyi"
+
+toggleNav();
